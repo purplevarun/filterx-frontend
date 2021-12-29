9fcab8e38241c01f9ff02d5cb57dd10c3ce7b351
purplevarun <purplevarun@gmail.com>
2021-12-29 23:57:06 +0530

    poster nd stuff added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,6 +3,14 @@ import React from 'react';
 const Contact = () => {
 	const link =
 		'https://docs.google.com/forms/d/e/1FAIpQLSfaN3AtrhjYb14hp_ZU17sY4jf9zo3QhwLLR1ZD6eUfrvqo5g/viewform?usp=sf_link';
+	function download(url) {
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = url.split('/').pop();
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	}
 	return (
 		<div style={{ textAlign: 'center' }}>
 			<div style={{ width: '100%', height: '5vh' }} />
@@ -14,6 +22,30 @@ const Contact = () => {
 				onClick={() => (window.location.href = link)}
 			>
 				Send us a message
+			</button>
+			<br />
+			<br />
+			<button
+				className="btn btn-sm btn-primary"
+				onClick={() => download('filterx-poster.pdf')}
+			>
+				Download Poster
+			</button>
+			<br />
+			<br />
+			<button
+				className="btn btn-sm btn-warning"
+				onClick={() => download('filterx-sticker.png')}
+			>
+				Download Sticker
+			</button>
+			<br />
+			<br />
+			<button
+				className="btn btn-sm btn-danger"
+				onClick={() => download('filterx-certificate.png')}
+			>
+				Download Certificate
 			</button>
 		</div>
 	);
